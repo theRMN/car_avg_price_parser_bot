edad23c0bc72d364bb2a9c5a616ab145531f831c
theRMN <roman.vassev@gmail.com>
2021-11-24 00:44:58 +0600

    added the ability to view all pages and check for a blank page

--- scripts.py
+++ scripts.py
@@ -2,22 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_avg_price(car: str, model: str, year: str):
-    url = f'https://aster.kz/cars/{car}/{model}?yearFrom={year}'
+def parse(pages, url):
 
+    total_price = 0
+    count = 0
+
+    for page_num in range(1, int(pages) + 1):
+        next_url = url + f'&page={page_num}'
+        full_page = requests.get(next_url)
+        soup = BeautifulSoup(full_page.content, 'html.parser')
+        price_list = soup.findAll('div', class_='price')
+        count += len(price_list)
+
+        for price in price_list:
+            price = int(price.next_element.replace(' ', ''))
+            total_price += price
+
+    return total_price / count
+
+
+def get_avg_price(car, model, year):
+    url = f'https://aster.kz/cars/{car}/{model}?yearFrom={year}'
     full_page = requests.get(url)
     soup = BeautifulSoup(full_page.content, 'html.parser')
-    price_list = soup.findAll('div', class_='price')
-    total_price = 0
+    button_text = soup.find('a', class_='btn btn_primary').text.strip()
 
-    for price in price_list:
-        price = int(price.next_element.replace(' ', ''))
-        total_price += price
+    if button_text == 'Показать 0 авто':
+        return None
 
-    avg_price = total_price / len(price_list)
+    try:
+        pages = soup.find('li', class_='page-item end').a.text
+        avg_price = parse(pages, url)
+    except AttributeError:
+        pages = 2
+        avg_price = parse(pages, url)
 
     return round(avg_price, 2)
 
 
 if __name__ == '__main__':
-    print(get_avg_price('acura', 'mdx', '2002'))
+    print(get_avg_price('audi', 'a4', '1993'))
