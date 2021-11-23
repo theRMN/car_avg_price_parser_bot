fb0f5e653e498c1924bc65774d64e22c41aaa385
theRMN <roman.vassev@gmail.com>
2021-11-23 22:41:30 +0600

    create function get_avg_price

--- scripts.py
+++ scripts.py
@@ -1,2 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+def get_avg_price(car: str, model: str, year: str):
+    url = f'https://aster.kz/cars/{car}/{model}?yearFrom={year}'
+
+    full_page = requests.get(url)
+    soup = BeautifulSoup(full_page.content, 'html.parser')
+    price_list = soup.findAll('div', class_='price')
+    total_price = 0
+
+    for price in price_list:
+        price = int(price.next_element.replace(' ', ''))
+        total_price += price
+
+    avg_price = total_price / len(price_list)
+
+    return round(avg_price, 2)
+
+
+if __name__ == '__main__':
+    print(get_avg_price('acura', 'mdx', '2002'))
